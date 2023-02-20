630cbf359ccba7e4f3524f9b88df3495ca768f9e
PhucLee2605 <phuc.lee.2605@gmail.com>
2023-02-20 13:18:35 +0700

    hot fixing metrics for evaluating recognition module

--- src/applications/features/speech_recognition/utils/metrics.py
+++ src/applications/features/speech_recognition/utils/metrics.py
@@ -5,17 +5,17 @@ from typing import List
 def cal_average_WER(predictions: List[str], references: List[str]):
     assert len(predictions) == len(
         references), "predictions and references must have the same number of elements"
-    wer_metric = load("wer")
+    wer = load("wer")
     total_wer = 0
 
     for i in range(len(predictions)):
-        total_wer += wer_metric.compute(predictions=predictions[i],
-                                        references=references[i])
+        total_wer += wer.compute(predictions=[predictions[i]],
+                                 references=[references[i]])
 
     return total_wer/len(predictions)
 
 
-def cal_single_WER(prediction: str, reference: str):
+def cal_single_WER(prediction: List[str], reference: List[str]):
     assert len(prediction) == len(
         reference), "predictions and references must have the same number of elements"
 
@@ -23,5 +23,5 @@ def cal_single_WER(prediction: str, reference: str):
         print("More than 1 element, redirecting to calculating average WER function")
         return cal_average_WER(prediction, reference)
 
-    wer_metric = load("wer")
-    return wer_metric.compute(predctions=prediction, references=reference)
+    wer = load("wer")
+    return wer.compute(predictions=prediction, references=reference)
