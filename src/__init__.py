807aa1c4c80121a41716d6a1bd3ba45bca4b4c22
Kiet Ngo <khoaimon180901@gmail.com>
2023-04-14 22:45:57 +0700

    modify front-end and add

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-
+    #for windows
+    os.environ['TRANSFORMERS_CACHE'] = 'D:/.cache/huggingface/hub'
     from .views import views
     from .auth import auth
 
