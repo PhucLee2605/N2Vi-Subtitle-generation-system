a293ff198e3da1cc17ffa5efa8abfb399ad92095
NgoAnhKiet <65819320+kietbg0079@users.noreply.github.com>
2023-04-24 12:45:13 +0700

    Update views.py

--- src/views.py
+++ src/views.py
@@ -188,7 +188,7 @@ def run_recognition():
 
                 text_output = ' '.join(predict)
             else:
-                text_output = ' '.join([line['text'] for line in transcription['chunks']])
+                text_output = '.'.join([line['text'] for line in transcription['chunks']]).replace('..', '.')
 
             txt_audio_output = f"{pretemp}/database/recognize/text/{audio_name}.txt"
 
